3a0ccbc7f644ea891f524ed349ce2f5f1e33fc62
S-F-III <mediocreacorn@gmail.com>
2024-11-07 14:46:18 -0600

    Added buttons to HamburgerActivity and its xml

--- app/src/main/java/com/example/dudewheresmycash/HamburgerActivity.java
+++ app/src/main/java/com/example/dudewheresmycash/HamburgerActivity.java
@@ -1,6 +1,9 @@
 package com.example.dudewheresmycash;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,86 @@ public class HamburgerActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button overview = findViewById(R.id.overviewButton);
+        Button expense = findViewById(R.id.expenseButton);
+        Button notification = findViewById(R.id.notificationButton);
+        Button accountInfo = findViewById(R.id.accountInfoButton);
+        Button settings = findViewById(R.id.settingsButton);
+        Button monthlySpending = findViewById(R.id.monthlySpendingButton);
+        Button signOut = findViewById(R.id.signOutButton);
+
+        overview.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                launchOverview();
+            }
+        });
+        expense.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                launchExpenses();
+            }
+        });
+        notification.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                launchNotfications();
+            }
+        });
+        accountInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchAccountInfo();
+            }
+        });
+        settings.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                launchSettings();
+            }
+        });
+        monthlySpending.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchMonthlySpending();
+            }
+        });
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchSignOut();
+            }
+        });
     }
+
+    private void launchOverview() {
+        Intent intent = new Intent(this, OverviewActivity.class);
+        startActivity(intent);
+    }
+    private void launchExpenses() {
+        Intent intent = new Intent(this, ExpenseActivity.class);
+        startActivity(intent);
+    }
+    private void launchNotfications() {
+        Intent intent = new Intent(this, NotificationActivity.class);
+        startActivity(intent);
+    }
+    private void launchAccountInfo() {
+        Intent intent = new Intent(this, AccountInfoActivity.class);
+        startActivity(intent);
+    }
+    private void launchSettings() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+    private void launchMonthlySpending() {
+        Intent intent = new Intent(this, MonthlySpendingActivity.class);
+        startActivity(intent);
+    }
+    private void launchSignOut() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
