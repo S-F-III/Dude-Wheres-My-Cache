1d0eb247a4e0164da4dfff82cb05847f5e7dbcdf
S-F-III <mediocreacorn@gmail.com>
2024-11-09 12:14:54 -0600

    added overlay to remaining Controller classes touched by HB menu

--- app/src/main/java/com/example/dudewheresmycash/NotificationActivity.java
+++ app/src/main/java/com/example/dudewheresmycash/NotificationActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,15 +26,85 @@ public class NotificationActivity extends AppCompatActivity {
         });
 
         ImageView hbMenu = findViewById(R.id.hbMenu);
-        hbMenu.setOnClickListener(new View.OnClickListener(){
+        ImageView hbMenu2 = findViewById(R.id.hbMenu2);
+        TextView overviewButton = findViewById(R.id.overviewButton);
+        TextView expenseButton = findViewById(R.id.expenseButton);
+        TextView notificationButton = findViewById(R.id.notificationButton);
+        TextView accountInfoButton = findViewById(R.id.accountInfoButton);
+        TextView settingsButton = findViewById(R.id.settingsButton);
+        TextView monthlySpendingButton = findViewById(R.id.monthlySpendingButton);
+        TextView signOutButton = findViewById(R.id.signOutButton);
+        hbMenu.setOnClickListener(v -> findViewById(R.id.hamburgerMenu).setVisibility(View.VISIBLE));
+        hbMenu2.setOnClickListener(v -> findViewById(R.id.hamburgerMenu).setVisibility(View.GONE));
+        overviewButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                launchHBMenu();
+                launchOverview();
+            }
+        });
+        expenseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                launchExpenses();
+            }
+        });
+        notificationButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                launchNotfications();
+            }
+        });
+        accountInfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchAccountInfo();
+            }
+        });
+        settingsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                launchSettings();
+            }
+        });
+        monthlySpendingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchMonthlySpending();
+            }
+        });
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchSignOut();
             }
         });
     }
-    private void launchHBMenu(){
-        Intent intent = new Intent(this, HamburgerActivity.class);
+    private void launchOverview() {
+        Intent intent = new Intent(this, OverviewActivity.class);
+        startActivity(intent);
+    }
+    private void launchExpenses() {
+        Intent intent = new Intent(this, ExpenseActivity.class);
+        startActivity(intent);
+    }
+    private void launchNotfications() {
+        Intent intent = new Intent(this, NotificationActivity.class);
+        startActivity(intent);
+    }
+    private void launchAccountInfo() {
+        Intent intent = new Intent(this, AccountInfoActivity.class);
+        startActivity(intent);
+    }
+    private void launchSettings() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+    private void launchMonthlySpending() {
+        Intent intent = new Intent(this, MonthlySpendingActivity.class);
+        startActivity(intent);
+    }
+    private void launchSignOut() {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
